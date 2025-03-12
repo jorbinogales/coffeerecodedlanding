d375664d2c15db9b2fd76ffd8499ca4b5e902340
jorbinogales <jorbinogales@gmail.com>
2025-03-12 18:45:15 -0400

    FIRST COMMIT

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@splinetool/runtime'],
+    include: ['@splinetool/runtime'], // Asegura que Vite lo procese
   },
   build: {
     rollupOptions: {
-      external: ['@splinetool/runtime'],
+      external: [], // No excluir módulos importantes
     },
   },
 })
